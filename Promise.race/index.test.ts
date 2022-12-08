f5971c595edf190153c8ed24fe3f566f4fe2d768
tohsaka888 <156132264@qq.com>
2022-12-08 16:07:16 +0800

    🧪 test: 手写 `Promise.race`

--- Promise.race/index.test.ts
+++ Promise.race/index.test.ts
@@ -1,8 +1,9 @@
-import myPromiseRace from "./index.js";
+import myPromiseRaceJS from "./index.js";
 import { FakeTime } from "time";
 import { assertEquals } from "asserts";
+import myPromiseRaceTS from "./index.ts";
 
-Deno.test("手写Promise.race", () => {
+Deno.test("手写Promise.race-JS", () => {
   const time = new FakeTime();
   try {
     const promise1 = new Promise<number>((resolve, reject) => {
@@ -17,11 +18,49 @@ Deno.test("手写Promise.race", () => {
     });
     const promise3 = new Promise<number>((resolve) => {
       setTimeout(() => {
-        resolve(2);
+        resolve(3);
       }, 3000);
     });
 
-    myPromiseRace([promise1, promise2, promise3]).then((val: number) => {
+    myPromiseRaceJS([promise1, promise2, promise3]).then((val: number) => {
+      time.nextAsync();
+      assertEquals(val, 1);
+    });
+
+    myPromiseRaceJS([promise2, promise3]).then((val: number) => {
+      time.nextAsync();
+      assertEquals(val, 2);
+    });
+  } finally {
+    time.restore();
+  }
+});
+
+Deno.test("手写Promise.race-TS", () => {
+  const time = new FakeTime();
+  try {
+    const promise1 = new Promise<number>((resolve, reject) => {
+      setTimeout(() => {
+        reject(1);
+      }, 1000);
+    });
+    const promise2 = new Promise<number>((resolve) => {
+      setTimeout(() => {
+        resolve(2);
+      }, 2000);
+    });
+    const promise3 = new Promise<number>((resolve) => {
+      setTimeout(() => {
+        resolve(3);
+      }, 3000);
+    });
+
+    myPromiseRaceTS([promise1, promise2, promise3]).then((val: number) => {
+      time.nextAsync();
+      assertEquals(val, 1);
+    });
+
+    myPromiseRaceTS([promise2, promise3]).then((val: number) => {
       time.nextAsync();
       assertEquals(val, 2);
     });
