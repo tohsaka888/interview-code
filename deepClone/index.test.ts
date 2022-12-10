71bc63e3419f6f97541d1a982291056effa7d6ec
tohsaka888 <156132264@qq.com>
2022-12-10 14:38:31 +0800

    🧪 test: 手写深拷贝

--- deepClone/index.test.ts
+++ deepClone/index.test.ts
@@ -1,7 +1,8 @@
 import deepCloneJS from "./index.js";
+import deepCloneTS from "./index.ts";
 import { assertEquals, assertNotStrictEquals } from "asserts";
 
-Deno.test("手写深拷贝", () => {
+Deno.test("手写深拷贝-JS", () => {
   const obj = {
     num: 1,
     str: "str",
@@ -19,6 +20,30 @@ Deno.test("手写深拷贝", () => {
   };
 
   const clonedObj = deepCloneJS(obj);
+
+  assertEquals(clonedObj, obj);
+  assertNotStrictEquals(clonedObj, obj);
+});
+
+
+Deno.test("手写深拷贝-TS", () => {
+  const obj = {
+    num: 1,
+    str: "str",
+    boolean: true,
+    und: undefined,
+    nul: null,
+    obj: { name: "对象", id: 1 },
+    arr: [0, 1, 2],
+    func: function () {
+      console.log("函数");
+    },
+    date: new Date(1),
+    reg: new RegExp("/正则/ig"),
+    [Symbol("1")]: 1,
+  };
+
+  const clonedObj = deepCloneTS(obj);
 
   assertEquals(clonedObj, obj);
   assertNotStrictEquals(clonedObj, obj);
